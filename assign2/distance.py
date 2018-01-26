f5998399409e117ed5e2280a2d133b2fa32541dd
Robin Cosbey <cosbeyr@wwu.edu>
2018-01-26 15:58:27 -0800

    Began adding recursion and handling errors.

--- assign2/distance.py
+++ assign2/distance.py
@@ -5,68 +5,53 @@ def distance(target, source, insertcost, deletecost, replacecost):
     m = len(source) + 1
 
     # set up dist and init values
-    dist = [ [(0,"") for j in range(m)] for i in range(n)]
+    dist = [ [0 for j in range(m)] for i in range(n)]
     for i in range(1,n):
-        dist[i][0] = dist[i-1][0][0] + insertcost, ""
+        dist[i][0] = dist[i-1][0] + insertcost
     for j in range(1,m):
-        dist[0][j] = dist[0][j-1][0] + deletecost, ""
+        dist[0][j] = dist[0][j-1] + deletecost
 
     # align source and target strings
     for j in range(1,m):
         #check(dist)
         for i in range(1,n):
-            inscost = insertcost + dist[i][j-1][0]
-            delcost = deletecost + dist[i-1][j][0]
+            inscost = insertcost + dist[i][j-1]
+            delcost = deletecost + dist[i-1][j]
             add = 0 if source[j-1] == target[i-1] else replacecost
-            substcost = add + dist[i-1][j-1][0]
-            dist[i][j] = min_op(inscost, delcost, substcost, add)
+            substcost = add + dist[i-1][j-1]
+            dist[i][j] = min(inscost, delcost, substcost)
 
     # return min edit distance
     return dist
 
-def min_op(inscost, delcost, substcost, add):
+def min_op(dist, i, j):
+    coordinates = []
+    inscost = dist[i][j-1]
+    delcost = dist[i-1][j]
+    substcost = dist[i-1][j-1]
+
     minimum = min(inscost, delcost, substcost)
-    op = None
-    if minimum == substcost:
-        op = "S" if add > 0 else "A" 
-    elif minimum == delcost:
-        op = "D"
-    elif minimum == inscost:
-        op = "I"
-   
-    return minimum, op
 
-def backtrace(dist, m, n):
-    ops = []
-    j = m - 1
-    i = n - 1
+    if inscost == minimum:
+        coordinates.append((i,j-1,"I"))
+    if delcost == minimum:
+        coordinates.append((i-1,j, "D"))
+    if substcost == minimum:
+        coordinates.append((i-1,j-1, "S" if dist[i][j] != substcost else "A"))
 
-    while i > 0 and j > 0:
-        op = dist[i][j][1]
-        ops.append(op)
-        if op == "I":
-            j = j - 1
-        elif op == "D":
-            i = i - 1
-        else:
-            i = i -1
-            j = j -1
+    return coordinates
+
+def backtrace(dist, i, j, ops, paths):
+
+    if i == 0 or j == 0:
+        paths.append(list(reversed(ops)))
+        return
     
-    inscost = dist[i][j-1][0]
-    delcost = dist[i-1][j][0]
-    substcost = dist[i-1][j-1][0]
-
-    minimum = min(inscost, delcost, substcost)
-
-    if minimum == substcost:
-        ops.append("S")
-    elif minimum == delcost:
-        ops.append("D")
-    else:
-        ops.append("I")
-
-    return list(reversed(ops))
-
+    coordinates = min_op(dist, i, j)
+    for coord in coordinates:
+        next_i, next_j, op = coord
+        backtrace(dist, next_i, next_j, ops + [op], paths)
+        
 def visualize(source, target, ops):
     t = 0
     s = 0
@@ -91,6 +76,7 @@ def visualize(source, target, ops):
             tar += target[t] + " "
             t += 1
             mid += "| " if op == "A" else "  "
+
     while t < len(target):
         tar += target[t] + " "
         t += 1
@@ -112,8 +98,11 @@ if __name__ == '__main__':
         n = len(argv[1]) + 1
         m = len(argv[2]) + 1
         dist = distance(argv[1], argv[2], 1, 1, 2)
-        print "levenshtein distance =", dist[n-1][m-1][0]
-        ops = backtrace(dist, m, n)
-        visualize(argv[1], argv[2], ops)
+        print "levenshtein distance =", dist[n-1][m-1]
+        paths = []
+        backtrace(dist, n-1, m-1, [], paths)
+        # check(dist)
+        print "paths", paths
+        #visualize(argv[1], argv[2], ops)
        
 
