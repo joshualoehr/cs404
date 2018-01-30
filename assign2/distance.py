6e8829239da1db2a5d510ac202a810d6fcc02ee5
Josh Loehr <loehrj@wwu.edu>
2018-01-29 17:57:41 -0800

    Finished assignment 2 part 1

--- assign2/distance.py
+++ assign2/distance.py
@@ -1,72 +1,123 @@
+################################################################################
+# CSCI 404 - Assignment 2 - Josh Loehr & Robin Cosbey - distance.py            # 
+#                                                                              # 
+# Computes minimum number of edits (insertions, deletions, substitutions) that #
+# can convert an input source string to an input target string. Outputs the    #
+# minimum edit Levenshtein distance, and N visualizations of equivalent        #
+# alignments.                                                                  #
+#                                                                              # 
+# usage: distance.py [-h] [-n N] target source                                 #
+#                                                                              # 
+# positional arguments:                                                        #
+#       target      The input target string.                                   #
+#       source      The input source string.                                   #
+#                                                                              # 
+# optional arguments:                                                          #
+#       -h, --help  show this help message and exit                            #
+#       -n N        The maximum number of alignments to print.                 #
+#                                                                              #
+################################################################################
 
+import argparse
 
 def argumentParser():
     parser = argparse.ArgumentParser()
-    parser.add_argument(target, type=str, help="The input target string.")
-    parser.add_argument(source, type=str, help="The input source string.")
-    parser.add_argument(-n, type=int, default=50, help="The maximum number of alignments to print.")
+    parser.add_argument('target', type=str, help="The input target string.")
+    parser.add_argument('source', type=str, help="The input source string.")
+    parser.add_argument('-n', type=int, default=50, help="The maximum number of alignments to print.")
     return parser.parse_args()
 
-def distance(target, source, insertcost, deletecost, replacecost):
-    n = len(target) + 1
-    m = len(source) + 1
+def distance(insertcost, deletecost, replacecost):
+    """Populates the dist matrix with the results of the minimum edit distance algorithm
+    Args:
+    insertcost -- cost of insertion operation
+    deletecost -- cost of deletion operation
+    replacecost -- cost of substitution operation
+    """
+    tlen = len(target) + 1
+    slen = len(source) + 1
 
-    # set up dist and init values
-    dist = [ [0 for j in range(m)] for i in range(n)]
-    for i in range(1,n):
-        dist[i][0] = dist[i-1][0] + insertcost
-    for j in range(1,m):
-        dist[0][j] = dist[0][j-1] + deletecost
+    dist = [ [0 for t in range(tlen)] for s in range(slen)]
+    for t in range(1,tlen):
+        dist[0][t] = dist[0][t-1] + insertcost
+    for s in range(1,slen):
+        dist[s][0] = dist[s-1][0] + deletecost
 
-    # align source and target strings
-    for j in range(1,m):
-        #check(dist)
-        for i in range(1,n):
-            inscost = insertcost + dist[i][j-1]
-            delcost = deletecost + dist[i-1][j]
-            add = 0 if source[j-1] == target[i-1] else replacecost
-            substcost = add + dist[i-1][j-1]
-            dist[i][j] = min(inscost, delcost, substcost)
+    for s in range(1,slen):
+        for t in range(1,tlen):
+            inscost = insertcost + dist[s-1][t]
+            delcost = deletecost + dist[s][t-1]
+            add = 0 if source[s-1] == target[t-1] else replacecost
+            substcost = add + dist[s-1][t-1]
+            dist[s][t] = min(inscost, delcost, substcost)
 
-    # return min edit distance
     return dist
 
-def min_op(dist, i, j):
+def min_op(dist, s, t, insertcost, deletecost, replacecost):
+    """Returns a list of triples containing the coordinates and operation symbol
+        of the next step in the backtrace of the dist matrix
+    Args:
+    dist -- the minimum edit distance matrix
+    s -- current dist matrix index corresponding to the source string
+    t -- current dist matrix index corresponding to the target string
+    insertcost -- cost of insertion operation
+    deletecost -- cost of deletion operation
+    replacecost -- cost of substitution operation
+    """
     coordinates = []
-    inscost = dist[i][j-1]
-    delcost = dist[i-1][j]
-    substcost = dist[i-1][j-1]
 
-    minimum = min(inscost, delcost, substcost)
+    inscost = dist[s][t-1] + insertcost
+    delcost = dist[s-1][t] + deletecost
+    substcost = dist[s-1][t-1] + replacecost
 
-    if inscost == minimum:
-        coordinates.append((i,j-1,"I"))
-    if delcost == minimum:
-        coordinates.append((i-1,j, "D"))
-    if substcost == minimum:
-        coordinates.append((i-1,j-1, "S" if dist[i][j] != substcost else "A"))
+    if inscost == dist[s][t]:
+        coordinates.append((s,t-1,"I"))
+    if delcost == dist[s][t]:
+        coordinates.append((s-1,t, "D"))
+    if substcost == dist[s][t]:
+        coordinates.append((s-1,t-1, "S" if replacecost > 0 else "A"))
 
     return coordinates
 
-def backtrace(dist, i, j, ops, paths):
+def backtrace(dist, s, t, ops, paths):
+    """Recursively traverses the dist matrix collecting minimum edit alignments
+    Args:
+    dist -- the minimum edit distance matrix
+    s -- current dist matrix index corresponding to the source string
+    t -- current dist matrix index corresponding to the target string
+    ops -- accumulated list of operations for a single path
+    paths -- list of all accumulated ops lists
+    """
 
-    if i == 0 or j == 0:
-        paths.append(list(reversed(ops)))
+    if s == 0 or t == 0:
+        if s > 0:
+             ops.append("D")
+        elif t > 0:
+             ops.append("I")
+        cost = sum([1 for op in ops if op == 'I' or op == 'D'])
+        cost += sum([2 for op in ops if op == 'S'])
+        if cost == dist[-1][-1]:
+            paths.append(list(reversed(ops)))
         return
     
-    coordinates = min_op(dist, i, j)
+    coordinates = min_op(dist, s, t, 1, 1, 0 if source[s-1] == target[t-1] else 2)
     for coord in coordinates:
-        next_i, next_j, op = coord
-        backtrace(dist, next_i, next_j, ops + [op], paths)
+        next_s, next_t, op = coord
+        backtrace(dist, next_s, next_t, ops + [op], paths)
         
-def visualize(source, target, ops):
+def visualize(path):
+    """Prints the alignment of the target and source strings based on
+        a given path
+    Args:
+    path -- a single accumulated list of operations through the dist matrix
+    """
     t = 0
     s = 0
     tar = ""
     sou = ""
     mid = ""
 
-    for op in ops:
+    for op in path:
         if op == "I":
             sou += "_ "
             tar += target[t] + " "
@@ -92,30 +143,23 @@ def visualize(source, target, ops):
         sou += source[s] + " "
         s += 1
 
-    print "%s\n%s\n%s" % (sou, mid, tar)
-
-def check(dist):
-    print
-    for row in reversed(dist):
-        print " ".join(["%2s(%s)" % el for el in row])
+    print "%s\n%s\n%s" % (tar, mid, sou)
 
 if __name__ == '__main__':
-#    from sys import argv
-#    if len(argv) > 2:
-#        n = len(argv[1]) + 1
-#        m = len(argv[2]) + 1
-
     args = argumentParser()
     source = args.source
     target = args.target
-    n = len(source) + 1
-    m = len(target) + 1
-    dist = distance(target, source, 1, 1, 2)
-    print "levenshtein distance =", dist[n-1][m-1]
+
+    slen = len(source) + 1
+    tlen = len(target) + 1
+    dist = distance(1, 1, 2)
+    print "levenshtein distance =", dist[slen-1][tlen-1]
+
     paths = []
-    backtrace(dist, n-1, m-1, [], paths)
-    # check(dist)
-    print "paths", paths
-    #visualize(argv[1], argv[2], ops)
+    backtrace(dist, slen-1, tlen-1, [], paths)
+
+    for path in paths[:args.n]:
+       visualize(path)
+       print
        
 
