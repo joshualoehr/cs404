e1e8af21f5b1d93be6cb7122b0aad4a47dbae5a5
Josh Loehr <loehrj@wwu.edu>
2018-02-27 17:52:19 -0800

    Refactored smoothing, fixed evaluation bug

--- assign5/vtag.py
+++ assign5/vtag.py
@@ -19,7 +19,7 @@ def txtfile(filepath):
 
 class Viterbi(object):
 
-    def __init__(self, train, raw, onecount=False):
+    def __init__(self, train, raw, smoothing="onecount"):
         self.train = train
         self.raw_vocab = nltk.FreqDist([word for word,_ in train] + raw)
         self.sing_tt_counts = {}
@@ -27,7 +27,16 @@ class Viterbi(object):
         self.word_counts = nltk.FreqDist([word for word,_ in train]) 
         self.tag_counts  = nltk.FreqDist([tag for _,tag in train])
         self.word_tag_counts = nltk.FreqDist(train)
-        self.tt_probs, self.tw_probs = self.calculate_probabilities(onecount=onecount)
+        
+        self.smoothing = smoothing
+        if not smoothing:
+            self.tt_smooth, self.tw_smooth = self.no_tt_smooth, self.no_tw_smooth
+        elif smoothing == 'onecount':
+            self.tt_smooth, self.tw_smooth = self.onecount_tt_smooth, self.onecount_tw_smooth
+        elif smoothing == 'addone':
+            self.tt_smooth, self.tw_smooth = self.addone_tt_smooth, self.addone_tw_smooth
+
+        self.tt_probs, self.tw_probs = self.calculate_probabilities()
         
     def sing_tt(self, prev_tag):
         if prev_tag not in self.sing_tt_counts:
@@ -47,53 +56,83 @@ class Viterbi(object):
     def backoff_tw_prob(self, word):
         return (self.word_counts[word] + 1) / (len(self.train) + len(self.raw_vocab) + 1)
 
-    def calculate_probabilities(self, onecount=False):
-        tt_probs = {}
-        tw_probs = {}
+    def calculate_probabilities(self):
+        tt_probs, tw_probs = {}, {}
 
         tag_bigrams = nltk.bigrams([tag for _,tag in self.train])
-        tag_bigram_counts = nltk.FreqDist(tag_bigrams)
+        self.tag_bigram_counts = nltk.FreqDist(tag_bigrams)
 
         for prev_gram, next_gram in nltk.bigrams(self.train):
             _, prev_tag = prev_gram
             next_word, next_tag = next_gram
             tag_bigram = (prev_tag, next_tag)
-
-            if not onecount:
-                if not tt_probs.get(tag_bigram):
-                    tt_probs[tag_bigram] = (tag_bigram_counts[tag_bigram]+1) / (self.tag_counts[prev_tag]+len(self.tag_counts))
-
-                if not tw_probs.get(next_gram):
-                    if next_gram == ('###','###'):
-                        tw_probs[next_gram] = self.word_tag_counts[next_gram] / self.tag_counts[next_tag]
-                    else:
-                        tw_probs[next_gram] = (self.word_tag_counts[next_gram]+1) / (self.tag_counts[next_tag]+len(self.tag_counts))
-
-            else:
-                if not tt_probs.get(tag_bigram):
-                    lam = 1 + self.sing_tt(prev_tag)
-                    backoff = lam * self.backoff_tt_prob(next_tag)
-                    tt_probs[tag_bigram] = (tag_bigram_counts[tag_bigram]+backoff) / (self.tag_counts[prev_tag]+lam)
-
-                if not tw_probs.get(next_gram):
-                    if next_gram == ('###','###'):
-                        tw_probs[next_gram] = self.word_tag_counts[next_gram] / self.tag_counts[next_tag]
-                    else:
-                        lam = 1 + self.sing_tw(next_tag)
-                        backoff = lam * self.backoff_tw_prob(next_word)
-                        tw_probs[next_gram] = (self.word_tag_counts[next_gram]+backoff) / (self.tag_counts[next_tag]+lam)
+            
+            if not tt_probs.get(tag_bigram):
+                tt_probs[tag_bigram] = self.tt_smooth(tag_bigram)
+            if not tw_probs.get(next_gram):
+                tw_probs[next_gram] = self.tw_smooth(next_gram)
 
         return tt_probs, tw_probs
 
+    def no_tt_smooth(self, tag_bigram):
+        prev_tag, _ = tag_bigram
+        return self.tag_bigram_counts[tag_bigram] / self.tag_counts[prev_tag]
+
+    def no_tw_smooth(self, next_gram):
+        _, next_tag = next_gram
+        return self.word_tag_counts[next_gram] / self.tag_counts[next_tag]
+
+    def addone_tt_smooth(self, tag_bigram):
+        prev_tag, _ = tag_bigram
+        vocab_size = len(self.tag_counts)
+        return (self.tag_bigram_counts[tag_bigram] + 1) / (self.tag_counts[prev_tag] + vocab_size)
+
+    def addone_tw_smooth(self, next_gram):
+        _, next_tag = next_gram
+        vocab_size = len(self.tag_counts)
+        if next_gram == ('###','###'):
+            return self.no_tw_smooth(next_gram)
+        else:
+            return (self.word_tag_counts[next_gram] + 1) / (self.tag_counts[next_tag] + vocab_size)
+
+    def onecount_tt_smooth(self, tag_bigram):
+        prev_tag, next_tag = tag_bigram
+        lam = 1 + self.sing_tt(prev_tag)
+        backoff = lam * self.backoff_tt_prob(next_tag)
+        return (self.tag_bigram_counts[tag_bigram] + backoff) / (self.tag_counts[prev_tag] + lam)
+
+    def onecount_tw_smooth(self, next_gram):
+        next_word, next_tag = next_gram
+        if next_gram == ('###','###'):
+            return self.no_tw_smooth(next_gram)
+        else:
+            lam = 1 + self.sing_tw(next_tag)
+            backoff = lam * self.backoff_tw_prob(next_word)
+            return (self.word_tag_counts[next_gram] + backoff) / (self.tag_counts[next_tag] + lam)
+
     def possible_tags(self, w):
         tags = [tag for (word,tag),count in self.word_tag_counts.items() if word == w]
-        if not tags:
-            return [tag for tag in self.tag_counts if tag != '###']
-        else:
-            return tags
+        return tags if tags else [tag for tag in self.tag_counts if tag != '###']
 
-    def unknown_prob(self, tag):
-        return 1 / (self.tag_counts[tag] + len(self.tag_counts))
+    def unknown_tt_prob(self, prev_tag, next_tag):
+        if not self.smoothing:
+            return -1
+        if self.smoothing == 'addone':
+            return 1 / (self.tag_counts[next_tag] + len(self.tag_counts))
+        elif self.smoothing == 'onecount':
+            lam = 1 + self.sing_tt(prev_tag)
+            backoff = lam * self.backoff_tt_prob(next_tag)
+            return backoff / (self.tag_counts[prev_tag] + lam)
+
+    def unknown_tw_prob(self, word, tag):
+        if not self.smoothing:
+            return -1
+        if self.smoothing == 'addone':
+            return 1 / (self.tag_counts[tag] + len(self.tag_counts))
+        elif self.smoothing == 'onecount':
+            lam = 1 + self.sing_tw(tag)
+            backoff = lam * self.backoff_tw_prob(word)
+            return backoff / (self.tag_counts[tag] + lam)
 
     def decode(self, sequence):
         sequence = sequence + ['###']
@@ -119,11 +158,11 @@ class Viterbi(object):
 
                 for prev_tag in prev_tag_dict:
 
-                    tt_prob = self.tt_probs.get((prev_tag, tag), self.unknown_prob(tag))
-                    tw_prob = self.tw_probs.get((word, tag), self.unknown_prob(tag))
+                    tt_prob = self.tt_probs.get((prev_tag, tag), self.unknown_tt_prob(prev_tag, tag))
+                    tw_prob = self.tw_probs.get((word, tag), self.unknown_tw_prob(word, tag))
                     p = log(tt_prob * tw_prob)
                     v = viterbi_probs[j-1][prev_tag][0] + p
-                    if v > viterbi_probs[j][tag][0]:
+                    if v >= viterbi_probs[j][tag][0]:
                         viterbi_probs[j][tag] = (v, prev_tag)
                     
 
@@ -139,6 +178,8 @@ class Viterbi(object):
         
     def evaluate(self, test_seq, tag_seq):
         sequence = [(word, test_tag, pred_tag) for (word, test_tag), pred_tag in zip(test_seq, tag_seq) ]
+        sequence = sequence[1:] # ignore SOS token
+
         overall_count, known_count, novel_count = 0, 0, 0
         known_total, novel_total = 0, 0
         for word, test_tag, pred_tag in sequence:
@@ -165,10 +206,12 @@ if __name__ == '__main__':
             help="The tagged testing data, with a single word/tag pair per line.")
     parser.add_argument("raw", type=txtfile,
             help="The raw untagged data, with a single word per line.")
+    parser.add_argument("--smoothing", type=str, choices=["addone", "onecount"], required=False)
     args = parser.parse_args()
 
-    viterbi = Viterbi(args.train, args.raw, onecount=True)
+    viterbi = Viterbi(args.train, args.raw, smoothing=args.smoothing)
     tag_seq = viterbi.decode([word for word,_ in args.test])
     print(tag_seq)
+    print([tag for _,tag in args.test])
     viterbi.evaluate(args.test, tag_seq)
     
