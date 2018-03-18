5d5642950a589877eed14a1a7b2eada462c16b86
Robin Cosbey <cosbeyr@wwu.edu>
2018-03-18 13:58:50 -0700

    Bug fixes and some cleanup after runs with simulated data.

--- final/model/basic_rnn.py
+++ final/model/basic_rnn.py
@@ -10,19 +10,18 @@ lens     == number of sentences contained in each document
 
 import argparse
 import numpy as np
+import tensorflow as tf
+import os
 
 def parseArgs():
     parser = argparse.ArgumentParser()
     
     # data
-    parser.add_argument("--train_dir", required=True, type=str,
-        help="Directory containing train .npy binary files.")
-    parser.add_argument("--dev_dir",   required=True, type=str,
-        help="Directory containing dev .npy binary files.")
-    parser.add_argument("--test_dir",  required=True, type=str,
-        help="Directory containing test .npy binary files.")
-    parser.add_argument("--out_dir",   required=True, type=str,
+    parser.add_argument("--data", required=True, type=str,
+        help="Directory containing train, dev, and test subdirectories.")
+    parser.add_argument("--out",   required=True, type=str,
         help="Directory to store produced summaries to.")
+    
     # hyperparameter values
     parser.add_argument("-lr", type=float, default=0.001, help="The learning rate.")
     parser.add_argument("-L",  type=int,   default=100,   help="The hidden layer dimension.")
@@ -32,18 +31,18 @@ def parseArgs():
     return parser.parse_args()
 
 def loadData(directory, set_name):
-    directory = args.directory + "/" + set_name if args.directory[-1] != "/" else args.directory + set_name
-    labels = np.load(directory + "_labels.npy")
-    features = np.load(directory + "_feats.npy")
-    all_lens = list(np.load(directory + "_lens.npy"))   # the number of sentences in each document
+    dir_file = directory + set_name + "/" + set_name
+    labels = np.load(dir_file + "_labels.npy")
+    features = np.load(dir_file + "_feats.npy")
+    all_lens = list(np.load(dir_file + "_lens.npy"))   # the number of sentences in each document
     
-    abstracts = list(np.load(directory + "_abs.npy")) if set_name == "test" else ""
-    documents = np.load(directory + "_docs.npy") if set_name == "test" else ""
-    doc_names = np.load(directory + "_names.npy") if set_name == "test" else ""
-        
+    abstracts = list(np.load(dir_file + "_abs.npy")) if set_name == "test" else ""
+    documents = np.load(dir_file + "_docs.npy") if set_name == "test" else ""
+    doc_names = np.load(dir_file + "_names.npy") if set_name == "test" else ""
+
     return labels, features, all_lens, abstracts, documents, doc_names
  
-def buildGraph(args, V):
+def buildGraph(args, vocab_size, num_feats):
     """
     """
     rnn_cell = tf.nn.rnn_cell.BasicRNNCell(args.L)
@@ -51,24 +50,23 @@ def buildGraph(args, V):
     # placeholders
     h0     = tf.placeholder(dtype=tf.float32, shape=(None, args.L), name="h0")
     lens   = tf.placeholder(dtype=tf.int64, shape=(None,), name="lens")
-    x = tf.placeholder(dtype=tf.int64, shape=(None, None, None), name="x")
+    x = tf.placeholder(dtype=tf.float32, shape=(None, None, num_feats), name="x")
     y_true = tf.placeholder(dtype=tf.int64, shape=(None, None), name="y_true")
     
     # outputs (mb, num_steps, args.L) - per time outputs for each sentence
     # state   (mb, args.L)            - last hidden state per sentence
     outputs, state = tf.nn.dynamic_rnn(cell=rnn_cell, 
                                        inputs=x,
-                                       sequence_lengths=lens,
+                                       sequence_length=lens,
                                        initial_state=h0,
                                        dtype=tf.float32)
-    
     # hidden to output layer weights (hidden layer dimension by vocabulary size)
     W = tf.get_variable(dtype=tf.float32,
-                                shape=(args.L, V),
+                                shape=(args.L, vocab_size),
                                 initializer=tf.glorot_uniform_initializer(),
                                 name="W")
     b = tf.get_variable(dtype=tf.float32,
-                                shape=(V),
+                                shape=(vocab_size),
                                 initializer=tf.glorot_uniform_initializer(),
                                 name="b")
 
@@ -76,7 +74,7 @@ def buildGraph(args, V):
     logits = tf.add(tf.tensordot(outputs, W, [[2],[0]]), b, name="logits")
 
     # mask containing only valid y_true
-    mask = tf.sequence_mask(lens, maxlen=tf.shape(x_ints)[1], dtype=tf.float32, name="mask")
+    mask = tf.sequence_mask(lens, maxlen=tf.shape(x)[1], dtype=tf.float32, name="mask")
 
     # compute loss over all num_steps and all sequences in the minibatch
     losses = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y_true, logits=logits)
@@ -89,95 +87,100 @@ def buildGraph(args, V):
 
     # perplexity and accuracy
     ppl = tf.exp(normalized_objective, name="perplexity")
-    acc = tf.reduce_mean(tf.cast(tf.equal(y_true, tf.argmax(logits, axis=2)), tf.float32), name="accuracy")
-    
+    max_logits = tf.argmax(logits, axis=2, name="ls")
+    acc = tf.reduce_mean(tf.cast(tf.equal(y_true, max_logits), tf.float32), name="accuracy")
 
     init = tf.global_variables_initializer()
 
     return init
 
-   
 def evaluateDev(args, features, labels, lens, sess):
     hidden_states = np.zeros(shape=(features.shape[0], args.L), dtype=np.float32)
-    
-    ppl, acc = sess.run(fetches=["perplexity:0","accuracy:0"],
-                        feed_dict={"x_ints:0":features[:,:,:]
-                                   "y_true:0":labels[:,:]
-                                   "h0:0":hidden_states,
-                                   "lens:0":(lens)})
-    print("Dev ppl = %.5f acc = %.5f" % (ppl, acc))
-
+    for row in range(features.shape[0]):    # row corresponds to document
+        x = np.reshape(features[row,:,:], (1, features.shape[1], features.shape[2]))
+        y_true = np.reshape(labels[row,:], (1, labels.shape[1]))
+        h = np.reshape(hidden_states[row,:], (1, hidden_states.shape[1]))
+        ppl, acc = sess.run(fetches=["perplexity:0","accuracy:0"],
+                            feed_dict={"x:0":x,
+                                       "y_true:0":y_true,
+                                       "h0:0":h,
+                                       "lens:0":([lens[row]])})
+        print("Dev ppl = %.5f acc = %.5f" % (ppl, acc))
+ 
 def generateSummary(logits, abstract_len, document, doc_name, out_dir):
-    out_fil = out_dir + "/" + doc_name if out_dir[-1] != "/" else outdir + doc_name
+    out_dir = out_dir + "/" if out_dir[-1] != "/" else out_dir
+    
+    if not os.path.exists(out_dir): 
+        os.makedirs(out_dir)
+
+    out_file = out_dir + doc_name + ".txt"
+    logits = logits.flatten('C')
     indexed_logits = list(enumerate(logits))
     indexed_logits.sort(key=lambda logit: logit[1])
-    sentence_indices = sorted([index for index, score in indexed_logits[:abstract_len])
+    sentence_indices = sorted([index for index, score in indexed_logits[:abstract_len]])
     
-    with open(out_fil, 'w') as f:
+    with open(out_file, 'w') as f:
         f.write("\n".join([document[i] for i in sentence_indices]))
 
 def evaluateTest(args, features, labels, lens, abstracts, documents, document_names, sess):
     hidden_states = np.zeros(shape=(features.shape[0], args.L), dtype=np.float32)
     for row in range(features.shape[0]):    # row corresponds to document
-        ppl, acc, logits = sess.run(fetches=["perplexity:0","accuracy:0", "logits:0"],
-                            feed_dict={"x_ints:0":sentences[row,:,:]
-                                       "y_true:0":labels[row,:]
-                                       "h0:0":hidden_states[row,:],
-                                       "lens:0":(lens[row])})
-        print("Test ppl = %.5f acc = %.5f" % (ppl, acc))
-        generateSummary(logits, abstracts[row], documents[row], document_names[row], args.out_dir)
+        x = np.reshape(features[row,:,:], (1, features.shape[1], features.shape[2]))
+        y_true = np.reshape(labels[row,:], (1, labels.shape[1]))
+        h = np.reshape(hidden_states[row,:], (1, hidden_states.shape[1]))
+        ppl, acc, logits = sess.run(fetches=["perplexity:0","accuracy:0", "ls:0"],
+                            feed_dict={"x:0":x,
+                                       "y_true:0":y_true,
+                                       "h0:0":h,
+                                       "lens:0":([lens[row]])})
+        print("%s: ppl = %.5f acc = %.5f" % (document_names[row], ppl, acc))
+        generateSummary(logits, abstracts[row], documents[row], document_names[row], args.out)
 
 def main():
     """
     Load datasets, train on minibatches for each epoch, and report scores on dev set.
     """
     args = parseArgs()
+    
+    data = args.data + "/" if args.data[-1] != "/" else args.data
 
     # load data
-    # labels = num_docs x num_sents
-    # feats = num_docs x num_sents x num_features
-    train_labels, train_feats, train_lens, _, _, _ = loadData(args.train_dir, "train")
-    dev_labels, dev_feats, dev_lens, _, _, _ = loadData(args.dev_dir, "dev")
-    test_labels, test_feats, test_lens, test_abs, test_docs, test_names = loadData(args.test_dir, "test")
+    train_labels, train_feats, train_lens, _, _, _ = loadData(data, "train")
+    dev_labels, dev_feats, dev_lens, _, _, _ = loadData(data, "dev")
+    test_labels, test_feats, test_lens, test_abs, test_docs, test_names = loadData(data, "test")
+
+    N = train_feats.shape[0]            # number of documents (number of training sequences)
+    num_steps = train_feats.shape[1]    # number of sentences, vocabulary size (length of the unrolled graph)
+    num_feats = train_feats.shape[2]    # number of features
     
-    num_steps = train_feats.shape[1]    # the length of our unrolled graph (max number of sentences)
-    N = train_feats.shape[0]            # number of training sequences (documents)
-    # vocab size - number of sentences in document (e.g. train_labels.shape[1])
-    V = train_labels.shape[1]
-
-    init = buildGraph(args, V)
-
+    init = buildGraph(args, num_steps, num_feats)
+ 
     # TRAIN
     with tf.Session() as sess:
         sess.run(fetches=[init])
 
-        num_batches = int(np.ceil(N/args.mb))
-        print("Total minibatches per epoch: %d" % num_batches)
+        print("Total minibatches per epoch: %d" % N)
 
-        for epoch in range(args.epochs):
+        for epoch in range(args.ep):
             print("epoch %d update... " % (epoch), end='', flush=True)
-            
-            for mb_row in range(num_batches):
-                row_start = mb_row*args.mb
-                row_end   = np.min([(mb_row+1)*args.mb, N]) # last minibatch may be partial
-
-                mb_x = train_feats[row_start:row_end, :, :]
-                mb_y = train_labels[row_start:row_end, :]
-                mb_lens = train_lens[row_start:row_end]
-                mb_h0 = np.zeros(shape=((), args.L), dtype=np.float32)
+            for mb_row in range(N):
+                mb_x = np.reshape(train_feats[mb_row,:,:], (1, num_steps, num_feats))
+                mb_y = np.reshape(train_labels[mb_row,:], (1, num_steps))
+                mb_lens = [train_lens[mb_row]]
+                mb_h0 = np.zeros(shape=(1, args.L), dtype=np.float32)
 
                 sess.run(fetches=["train_step"],
-                         feed_dict={"x_ints:0":mb_x,
-                                    "y_trues:0":mb_y,
+                         feed_dict={"x:0":mb_x,
+                                    "y_true:0":mb_y,
                                     "h0:0":mb_h0,
-                                    "lens:0":mb_lens})
+                                    "lens:0":(mb_lens)})
 
-                if(mb_row % 100 = 0):
+                if(mb_row % 100 == 0):
                     print("%d... " % (mb_row),end='',flush=True)
             print("training loop finished.")
     
             evaluate(args, dev_feats, dev_labels, dev_lens, sess)
-        evaluate(args, test_feats, test_labels, test_lens, test_abs, test_docs, test_names, sess)          
+        evaluateTest(args, test_feats, test_labels, test_lens, test_abs, test_docs, test_names, sess)          
 
 if __name__ == "__main__":
     main()
